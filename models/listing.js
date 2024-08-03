e80ba9d317c23575b6b3f1a46f93612446721218
Raisha58 <2058raisha@gmail.com>
2024-08-03 20:05:10 +0545

    Added create and new routes

--- models/listing.js
+++ models/listing.js
@@ -1,11 +1,38 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// // Define the schema for listing
+// const imageSchema = new Schema({
+//   filename: String,
+//   url: String,
+// });
+
+// const listingSchema = new Schema({
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+//   description: String,
+//   image: {
+//     type: S,
+//     default: {
+//       filename: "default-image.jpg",
+//       url: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+//     },
+//   },
+//   price: Number,
+//   location: String,
+//   country: String,
+// });
+
+// // Create the model for the schema
+// const Listing = mongoose.model("Listing", listingSchema);
+
+// // Export the model for use in other parts of the application
+// module.exports = mongoose.model("Listing", listingSchema);
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
-// Define the schema for listing
-const imageSchema = new Schema({
-  filename: String,
-  url: String,
-});
 
 const listingSchema = new Schema({
   title: {
@@ -14,19 +41,17 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
-    type: imageSchema,
-    default: {
-      filename: "default-image.jpg",
-      url: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-    },
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
   },
   price: Number,
   location: String,
   country: String,
 });
-
-// Create the model for the schema
 const Listing = mongoose.model("Listing", listingSchema);
-
-// Export the model for use in other parts of the application
-module.exports = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
