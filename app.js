0bb606338034c6d91ef94d56716c991fec09beaa
Raisha58 <2058raisha@gmail.com>
2024-09-15 21:02:03 +0545

    added handling of deleting reviews

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ app.delete(
 );
 
 // Reviews
-// Post Route
+// Post Review Route
 app.post(
   "/listings/:id/reviews",
   validateReview,
@@ -146,6 +146,18 @@ app.post(
     // res.send("new review saved");
 
     res.redirect(`/listings/${listing._id}`);
+  })
+);
+// Delete Review Route
+app.delete(
+  "listing/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listing/${id}`);
   })
 );
 
--- models/listing.js
+++ models/listing.js
@@ -1,38 +1,6 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-
-// // Define the schema for listing
-// const imageSchema = new Schema({
-//   filename: String,
-//   url: String,
-// });
-
-// const listingSchema = new Schema({
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   description: String,
-//   image: {
-//     type: S,
-//     default: {
-//       filename: "default-image.jpg",
-//       url: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-//     },
-//   },
-//   price: Number,
-//   location: String,
-//   country: String,
-// });
-
-// // Create the model for the schema
-// const Listing = mongoose.model("Listing", listingSchema);
-
-// // Export the model for use in other parts of the application
-// module.exports = mongoose.model("Listing", listingSchema);
-
 const mongoose = require("mongoose");
-const review = require("./review");
+// const review =require("./review");
+const Review = require("./review.js");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -60,5 +28,13 @@ const listingSchema = new Schema({
     },
   ],
 });
+
+// mongoose middleware that allows reviews to be deleted once listing is del
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
